f5ce99ff5f88ff41c22055080c55286521846240
nobuyuki ishii <nby2ki@gmail.com>
2013-01-09 23:49:50 +0900

    unnecessary file deleted

--- gen/jp/gr/java_conf/ishiisoft/android/sample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.gr.java_conf.ishiisoft.android.sample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
